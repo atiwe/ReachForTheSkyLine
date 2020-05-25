912718977b06481b88e578379c2cb391cde3ce49
Mzet <martindahren@gmail.com>
2020-05-25 11:31:00 +0200

    Not much added.

--- src/main/java/RFTSLgroup/RFTSLid/App.java
+++ src/main/java/RFTSLgroup/RFTSLid/App.java
@@ -1,6 +1,8 @@
 package RFTSLgroup.RFTSLid;
 
 
+import java.awt.print.Book;
+
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Cluster.Builder;
 import com.datastax.driver.core.Session;
@@ -11,6 +13,8 @@ public class App
     private Cluster cluster;
     
     private Session session;
+    
+    private static final String TABLE_NAME = "EMPLOYEES";
  
     public void connect(String node, Integer port) {
         Builder b = Cluster.builder().addContactPoint(node);
@@ -30,6 +34,61 @@ public class App
         session.close();
         cluster.close();
     }
+    
+/*    public void createKeySpace(String keySpaceName, String replicationStrategy, int replicationFactor)
+    {
+    	StringBuilder sb = new StringBuilder("CREATE KEYSPACE IF NOT EXISTS ").append(keySpaceName).append(" WITH replication = {")
+    			.append("'class':'").append(replicationStrategy).append("','replication_factor':")
+    			.append(replicationFactor).append("};");
+    	
+    	String query = sb.toString();
+    	session.execute(query);
+    }
+    
+    public void createTable()
+    {
+    	StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
+    			.append(TABLE_NAME).append("(")
+    			.append("id uuid PRIMARY KEY, ")
+    			.append("title text,")
+    			.append("subject text);");
+    	
+    	String query = sb.toString();
+    	session.execute(query);
+    }
+    
+    public void alterTableEmployee(String columnName, String columnType)
+    {
+    	StringBuilder sb = new StringBuilder("ALTER TABLE ")
+    			.append(TABLE_NAME).append(" ADD ")
+    			.append(columnName).append(" ")
+    			.append(columnType).append(";");
+    	
+    	String query = sb.toString();
+    	session.execute(query);
+    }
+    
+    public void insertEmployeeByTitle()
+    {
+    	StringBuilder sb = new StringBuilder("INSERT INTO ")
+    			.append(TABLE_NAME).append(" ADD ")
+    			.append(columnName).append()
+    			.append(columnType).append(";");
+    	
+    	String query = sb.toString();
+    	session.execute(query);
+    }
+/*    private KeyspaceRepository schemaRepository;
+    
+    @Before
+    public void connect()
+    {
+    	CassandraConnector client = new CassandraConnector();
+    	client.connect("127.0.0.1", 9042);
+    	this.session = client.getSession();
+    	schemaRepository = new KeyspaceRepository(session);
+    }*/
+    
     public static void main( String[] args )
     {
     	App app = new App();
